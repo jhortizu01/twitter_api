2bd698841bd1c6f02b3e07a98f64d39157e2f7fb
Matt Rowan <matt.rowan121@gmail.com>
2023-08-28 20:23:13 -0400

    Create controller advice

---
+++ src/main/java/com/cooksys/twitter_api/controllers/advice/SocialMediaControllerAdvice.java
@@ -0,0 +1,12 @@
+package com.cooksys.twitter_api.controllers.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice(basePackages = {"com.cooksys.twitter_api.controllers"})
+@ResponseBody
+public class SocialMediaControllerAdvice {
+
+
+
+}

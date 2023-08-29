b6f9e45aaa57713e3c25e0e8181725f3e667fc74
Matt Rowan <matt.rowan121@gmail.com>
2023-08-28 20:20:00 -0400

    Create validate controller

---
+++ src/main/java/com/cooksys/twitter_api/controllers/ValidateController.java
@@ -0,0 +1,12 @@
+package com.cooksys.twitter_api.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/validate")
+public class ValidateController {
+
+}

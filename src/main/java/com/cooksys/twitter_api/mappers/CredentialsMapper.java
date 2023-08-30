aabaebe52943e3e933fb0e2330fdd741a69d7d93
Janika Hortizuela <jhortizuela21@gmail.com>
2023-08-29 21:16:58 -0600

    Create CredentialsMapper

---
+++ src/main/java/com/cooksys/twitter_api/mappers/CredentialsMapper.java
@@ -0,0 +1,11 @@
+package com.cooksys.twitter_api.mappers;
+
+import com.cooksys.twitter_api.dtos.CredentialsDto;
+import com.cooksys.twitter_api.entities.embeddable.Credentials;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CredentialsMapper {
+
+    CredentialsDto entityToDto(Credentials credentials);
+}

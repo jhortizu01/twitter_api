9020a2f1c5c1a156d111b56f8113ee4be199a28f
Matt Rowan <matt.rowan121@gmail.com>
2023-09-01 14:28:45 -0400

    Add functionality to check if hashtag exists

--- src/main/java/com/cooksys/twitter_api/controllers/ValidateController.java
+++ src/main/java/com/cooksys/twitter_api/controllers/ValidateController.java
@@ -1,6 +1,9 @@
 package com.cooksys.twitter_api.controllers;
 
+import com.cooksys.twitter_api.services.ValidateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/validate")
 public class ValidateController {
+
+    private ValidateService validateService;
+
+    @GetMapping("/tag/exists/{label}")
+    public Boolean verifyHashtag(@PathVariable String label) {
+        return validateService.verifyHashtag(label);
+    }
 
 }
--- src/main/java/com/cooksys/twitter_api/services/impl/ValidateServiceImpl.java
+++ src/main/java/com/cooksys/twitter_api/services/impl/ValidateServiceImpl.java
@@ -1,10 +1,23 @@
 package com.cooksys.twitter_api.services.impl;
 
+import com.cooksys.twitter_api.entities.Hashtag;
+import com.cooksys.twitter_api.repositories.HashtagRepository;
 import com.cooksys.twitter_api.services.ValidateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class ValidateServiceImpl implements ValidateService {
+
+    private HashtagRepository hashtagRepository;
+
+    @Override
+    public Boolean verifyHashtag(String label) {
+        Optional<Hashtag> hashtag = hashtagRepository.findByLabel(label);
+        return hashtag.isPresent();
+    }
+
 }

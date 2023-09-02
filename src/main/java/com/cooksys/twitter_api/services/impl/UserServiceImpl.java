0cc88d525679829a6ab106ed4b37605d846a9ef8
Matt Rowan <matt.rowan121@gmail.com>
2023-09-01 22:28:49 -0400

    Add error handling message to cotroller advice

--- src/main/java/com/cooksys/twitter_api/services/impl/UserServiceImpl.java
+++ src/main/java/com/cooksys/twitter_api/services/impl/UserServiceImpl.java
@@ -23,100 +23,105 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-	private final UserRepository userRepository;
-	private final UserMapper userMapper;
-	private final TweetMapper tweetMapper;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final TweetMapper tweetMapper;
 
-	@Override
-	public List<UserResponseDto> getAllActiveUsers() {
-		var result = userRepository.findAll().stream().filter(user -> !user.isDeleted()).toList();
-		return userMapper.entitiesToDtos(result);
-	}
+    @Override
+    public List<UserResponseDto> getAllActiveUsers() {
+        var result = userRepository.findAll().stream().filter(user -> !user.isDeleted()).toList();
+        return userMapper.entitiesToDtos(result);
+    }
 
-	@Override
-	public List<TweetResponseDto> getFeed(String username) {
+    @Override
+    public List<TweetResponseDto> getFeed(String username) {
 
-		Optional<User> currentUser = userRepository.findByCredentialsUsername(username);
-		if(currentUser.isEmpty()) {
-			throw new NotFoundException("User does not exist");
-		}
-		
-		User user = currentUser.get();
-		List<Tweet> userFeed = new ArrayList<>(user.getTweets());
-		for (User u : user.getFollowing()) {
-			userFeed.addAll(u.getTweets());
-		}
-		return tweetMapper.entitiesToDtos(userFeed);
+        Optional<User> currentUser = userRepository.findByCredentialsUsername(username);
+        if (currentUser.isEmpty()) {
+            throw new NotFoundException("User does not exist");
+        }
 
-	}
+        User user = currentUser.get();
+        List<Tweet> userFeed = new ArrayList<>(user.getTweets());
+        for (User u : user.getFollowing()) {
+            userFeed.addAll(u.getTweets());
+        }
+        return tweetMapper.entitiesToDtos(userFeed);
 
-	@Override
-	public boolean validateUsername(String username) {
-		
-		Optional<User> userToFind = userRepository.findByCredentialsUsername(username);
-		
-		if(userToFind.isEmpty()) {
-			return true;
-		}else {
-			return false;
-		}
+    }
 
-	}
+    @Override
+    public boolean validateUsername(String username) {
 
-	@Override
-	public void unfollow(CredentialsDto username) {
+        Optional<User> userToFind = userRepository.findByCredentialsUsername(username);
 
-		
-		
-	}
-	
-	@Override
-	public void follow(CredentialsDto username) {
+        if (userToFind.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
 
-		
-		
-	}
-	
+    }
 
-	@Override
-	public List<TweetResponseDto> getMentions(Long id) {
-		User currentUser = userRepository.findById(id).get();
-		
-		List<Tweet> mentions = currentUser.getMentionedTweets();
-		
-		return tweetMapper.entitiesToDtos(mentions);
-	}
+    @Override
+    public void unfollow(CredentialsDto username) {
 
-	// TODO: User is being created but variable 'phoneNumber' is not populating
 
-	@Override
-	public UserResponseDto createUser(UserRequestDto userRequestDto) {
-		User user = getUserByUsername(userRequestDto.getCredentials().getUsername());
-		if (user == null) {
-			user = userMapper.userRequestDtoToEntity(userRequestDto);
-		} else if (user.isDeleted()) {
-			user.setCredentials(user.getCredentials());
-			user.setProfile(user.getProfile());
-			user.setDeleted(false);
-		}
-		else
-			throw new BadRequestException("Username must be unique");
-		validateUser(user);
-		return userMapper.entityToDto(userRepository.saveAndFlush(user));
-	}
+    }
 
-	private User getUserByUsername(String username) {
-		Optional<User> optionalUser = userRepository.findByCredentialsUsername(username);
+    @Override
+    public void follow(CredentialsDto username) {
+
+
+    }
+
+
+    @Override
+    public List<TweetResponseDto> getMentions(Long id) {
+        User currentUser = userRepository.findById(id).get();
+
+        List<Tweet> mentions = currentUser.getMentionedTweets();
+
+        return tweetMapper.entitiesToDtos(mentions);
+    }
+
+    // TODO: User is being created but variable 'phoneNumber' is not populating
+
+    @Override
+    public UserResponseDto createUser(UserRequestDto userRequestDto) {
+        User user = getUserByUsername(userRequestDto.getCredentials().getUsername());
+        if (user == null) {
+            user = userMapper.userRequestDtoToEntity(userRequestDto);
+        } else if (user.isDeleted()) {
+            user.setCredentials(user.getCredentials());
+            user.setProfile(user.getProfile());
+            user.setDeleted(false);
+        } else
+            throw new BadRequestException("Username must be unique");
+        validateUser(user);
+        return userMapper.entityToDto(userRepository.saveAndFlush(user));
+    }
+
+    private User getUserByUsername(String username) {
+        Optional<User> optionalUser = userRepository.findByCredentialsUsername(username);
         return optionalUser.orElse(null);
     }
 
-	private void validateUser(User user) {
-		if (user.getCredentials().getUsername() == null)
-			throw new BadRequestException("username is required.");
-		if (user.getCredentials().getPassword() == null)
-			throw new BadRequestException("password is required.");
-		if (user.getProfile().getEmail() == null)
-			throw new BadRequestException("email is required.");
-	}
+    private void validateUser(User user) {
+        if (user == null)
+            throw new BadRequestException("No credentials were provided. Please try again.");
+
+        if (user.getCredentials() == null)
+            throw new BadRequestException("No credentials were provided. Please try again.");
+
+        if (user.getProfile() == null)
+            throw new BadRequestException("No profile info was provided. Please try again.");
+
+        if (user.getCredentials().getUsername() == null || user.getCredentials().getPassword() == null)
+            throw new BadRequestException("Missing credentials. Please check the entered username and password.");
+
+        if (user.getProfile().getEmail() == null)
+            throw new BadRequestException("Missing profile info. Please check the entered info and try again.");
+    }
 
 }
